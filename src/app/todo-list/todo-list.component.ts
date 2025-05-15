98ac10a0ad85a262c13c060470184e16f3b0734b
ChristianMoraV <christianmoises15@hotmail.com>
2025-05-15 13:46:33 -0500

    Subida de taller

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.scss'
+})
+export class TodoListComponent {
+  tasks: string[] = ['Taller','Chrstian'];
+  newTask: string = '';
+
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+}
